f913612f7ca8d755889fcd9eda71a0636c5fa238
cayovalsamis <cayovalsamis@globo.com>
2015-11-25 23:10:55 -0200

    Codigo encontra os minimos
    O codigo está funcionando para encontrar os minimos da função mas por
    algum motivo não esta recebendo o numero de iterações, de qualquer forma
    a solução deve ser simples

--- Fibonacci.m
+++ Fibonacci.m
@@ -1,9 +1,9 @@
-function [minimo, numI] = Fibonacci(expr, inicio_intervalo, fim_intervalo, numero_reducoes, indice_fibonacci, tol)
+function [minimo, numI] = Fibonacci(expr, inicio_intervalo, fim_intervalo, numero_reducoes, tol)
     %dados
     a = inicio_intervalo;
     b = fim_intervalo;
     n = numero_reducoes;
-    k = indice_fibonacci;
+    k = n + 1;
     numI = 0;
    
     %operacoes
@@ -18,22 +18,22 @@ function [minimo, numI] = Fibonacci(expr, inicio_intervalo, fim_intervalo, numer
         Lini = b-a;
         x2 = alfa*x1 + (1-alfa)*x4;
         f2 = subs(expr,x,x2);
-        fx2 = eval(f);
+        fx2 = eval(f2);
         x3 = alfa*x4 + (1-alfa)*x1;
         f3 = subs(expr,x,x3);
-        fx3 = eval(f);
+        fx3 = eval(f3);
         if fx2 < fx3
             a = x1;
             b = x3;
             Lfin = b-a;
             if b-a < tol
-                minimo = fx2;
+                minimo = [fx2 x2];
                 break;
             end;
             alfa = (Lini - Lfin)/Lfin;
             numI = numI + 1;
             if numI >= n
-                minimo = fx2;
+                minimo = [fx2 x2];
                 break;
             end;
         else
@@ -41,13 +41,13 @@ function [minimo, numI] = Fibonacci(expr, inicio_intervalo, fim_intervalo, numer
             b = x4;
             Lfin = b-a;
             if b-a < tol
-                minimo = fx3;
+                minimo = [fx3 x3];
                 break;
             end;
             alfa = (Lini-Lfin)/Lfin;
             numI = numI+1;
             if numI >= n
-                minimo = fx3;
+                minimo = [fx3 x3];
                 break;
             end
         end
