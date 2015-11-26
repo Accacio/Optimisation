d9eaf2c5ef47d69c51637e351457db61a50f839b
RodrigoMoyses <rodrigomoyses52@gmail.com>
2015-11-26 00:42:49 -0300

    GUI update 1.2
    some tweaks, new features as tolerance and max iterations
    "razão áurea" on its baby steps, but working nevertheless

--- gui.m
+++ gui.m
@@ -22,10 +22,10 @@ function varargout = gui(varargin)
 
 % Edit the above text to modify the response to help gui
 
-% Last Modified by GUIDE v2.5 25-Nov-2015 18:23:21
+% Last Modified by GUIDE v2.5 25-Nov-2015 22:34:38
 
 % Begin initialization code - DO NOT EDIT
-gui_Singleton = 0;
+gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
                    'gui_OpeningFcn', @gui_OpeningFcn, ...
@@ -85,7 +85,10 @@ function func_Callback(hObject, eventdata, handles)
 %     errordlg('Entrada deve ser uma função','Erro','modal')
 %     return
 % end
-    
+func = get(hObject,'String');   %gets what's written on the func text box
+handles.func = string(func);    %sending the value of func to the handles struct
+set(hObject,'ForegroundColor',[0 0 0],'String',func); %making the correct number appear onscreen (with . instead of ,)
+guidata(hObject,handles) %sending the struct back to the program   
 
 % Hints: get(hObject,'String') returns contents of func as text
 %        str2double(get(hObject,'String')) returns contents of func as a double
@@ -111,6 +114,11 @@ function button_calc_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 h = get(handles.panel_method,'SelectedObject'); %store in h the radio button that is selected
 method = get(h,'String');    %store in method the name of the selected radio button
+xi = handles.num.xi;
+xf = handles.num.xf;
+func = handles.func;
+tol = handles.num.tol;
+it = handles.num.it;
 switch method
     case get(handles.button_fibo,'String')  %in case the name of the button is the same as the fibonacci button
         warndlg('Função selecionada: Fibonacci','Aviso');
@@ -118,17 +126,27 @@ switch method
         set(handles.result_x,'String','x = ponto de mínimo')
         set(handles.result_fx,'String','f(x) = valor do mínimo')
     case get(handles.button_aurea,'String') %in case the name of the button is the same as the aurea button
-        warndlg('Função selecionada: Razão Áurea','Aviso');
         % TODO call aurea function
-        set(handles.result_x,'String','x = ponto de mínimo')
+        [min, xi_lim, xf_lim, it_max] = aurea(func,xi,xf,tol,it);
+        min = num2str(min);
+        resultx = ['x = ' min];
+        set(handles.result_x,'String',resultx)
+        %set(handles.result_x,'String','x = ponto de mínimo')
         set(handles.result_fx,'String','f(x) = valor do mínimo')
+        if it_max == it
+            warning = 'Máximo de iterações. Intervalo final igual a [';
+            warning = [warning num2str(xi_lim)];
+            warning = [warning num2str(xf_lim)];
+            warning = [warning '].'];
+            warndlg(warning,'Aviso');
+        end
     case get(handles.button_poly,'String')  %in case the name of the button is the same as the polinomial button
         warndlg('Função selecionada: Interpolação Polinomial','Aviso');
         % TODO call polynomial interpolation function
         set(handles.result_x,'String','x = ponto de mínimo')
         set(handles.result_fx,'String','f(x) = valor do mínimo')
 end
-
+guidata(hObject,handles)
 
 
 function xi_Callback(hObject, eventdata, handles)
@@ -206,13 +224,13 @@ end
 
 
 
-function tolerance_Callback(hObject, eventdata, handles)
-% hObject    handle to tolerance (see GCBO)
+function tol_Callback(hObject, eventdata, handles)
+% hObject    handle to tol (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of tolerance as text
-%        str2double(get(hObject,'String')) returns contents of tolerance as a double
+% Hints: get(hObject,'String') returns contents of tol as text
+%        str2double(get(hObject,'String')) returns contents of tol as a double
 
 tol = get(hObject,'String');   %gets what's written on the tolerância text box
 tol(tol==',') = '.';      %substitutes the , for .
@@ -230,8 +248,43 @@ guidata(hObject,handles) %sending the struct back to the program
 
 
 % --- Executes during object creation, after setting all properties.
-function tolerance_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to tolerance (see GCBO)
+function tol_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to tol (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function it_Callback(hObject, eventdata, handles)
+% hObject    handle to it (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+it = get(hObject,'String');   %gets what's written on the it text box
+it(it==',') = '.';      %substitutes the , for .
+it = str2double(it);    %transforms into a double
+
+if isnan(it) %in case the user inputs something that isn't a number
+    set(hObject, 'String', '[erro]','ForegroundColor',[1 0 0]);
+    errordlg('it deve ser um número','Erro','modal');
+    return
+end
+
+handles.num.it = it;    %sending the value of it to the handles struct
+set(hObject,'ForegroundColor',[0 0 0],'String',it); %making the correct number appear onscreen (with . instead of ,)
+guidata(hObject,handles) %sending the struct back to the program
+% Hints: get(hObject,'String') returns contents of it as text
+%        str2double(get(hObject,'String')) returns contents of it as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function it_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to it (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
